de10415a48c7e42fef8453dc92b2a6a349a63eff
Sindre <sindre.glomnes@gmail.com>
2022-09-12 10:25:52 +0200

    oppgave 1

--- exercise4/main.cpp
+++ exercise4/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -5,6 +6,15 @@ using namespace std;
 
 int main() {
   vector<double> vec{1, 2, 3, 4, 5};
-  cout << vec.front() << endl;
-  cout << vec.back() << endl;
+  cout << "front: " << vec.front() << endl;
+  cout << "back: " << vec.back() << endl;
+
+  vec.emplace(vec.begin() + 1, 10);
+
+  double number = 5;
+  auto search = find(vec.begin(), vec.end(), number);
+
+  if (*search == number) {
+    cout << "Found the number: " << *search << endl;
+  }
 }

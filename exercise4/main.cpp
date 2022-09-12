3470ce77954b923c74f35dfb08ab72b621563f6e
Sindre <sindre.glomnes@gmail.com>
2022-09-12 10:11:44 +0200

    exercise 4 init

---
+++ exercise4/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<double> vec{1, 2, 3, 4, 5};
+  cout << vec.front() << endl;
+  cout << vec.back() << endl;
+}

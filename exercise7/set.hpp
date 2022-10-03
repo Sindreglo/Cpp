23d909c58fe172f048f36bf83b4897f4d96e72da
Sindre <sindre.glomnes@gmail.com>
2022-10-03 10:21:10 +0200

    cleanup

--- exercise7/set.hpp
+++ exercise7/set.hpp
@@ -18,8 +18,10 @@ public:
   friend ostream &operator<<(ostream &os, const Set &set);
 };
 
+// Lager ny tom mengde
 Set::Set() {}
 
+// Finner unionen av to mengder
 Set &Set::operator<<(const Set &other) {
   for (int integer : other.table)
     *this += integer;
@@ -34,17 +36,20 @@ bool Set::number_exists(int integer) const {
   return false;
 };
 
+// Legger til et nytt tall i en mengde
 Set &Set::operator+=(int integer) {
   if (!number_exists(integer))
     table.push_back(integer);
   return *this;
 };
 
+// Setter en mengde lik en annen mengde
 Set &Set::operator=(const Set &set) {
   table = set.table;
   return *this;
 };
 
+// Skriver ut en mengde
 ostream &operator<<(ostream &os, const Set &set) {
   os << "{ ";
   for (int i = 0; i < set.table.size() - 1; i++)
